c3739d1e3efee1fa6ab76143d51cacf91bdac3a9
root <root@box-761131.local>
2024-05-18 20:43:08 +0300

    .

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(root_path="/v1")
 
 
-@app.get("/api/v1")
+@app.get("/")
 async def hello_world() -> dict:
     return {"message": "hello world"}
