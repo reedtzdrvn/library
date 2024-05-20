5e8fb426a213ea90fec8f934548d43063e603a2c
Daniil Markov <reedtzhere@gmail.com>
2024-05-21 00:19:52 +0300

    finished authors page

---
+++ frontend/src/components/Authors/AuthorCard.jsx
@@ -0,0 +1,13 @@
+const AuthorCard = ({ name, image }) => {
+    return (
+      <div className="flex justify-center transition cursor-pointer group transition hover:text-[#e84565]">
+        <div>
+          <img className="w-[300px] h-[400px] rounded-md border-2 transition group-hover:border-[#e84565]" src={image} />
+          <span className="2xl:text-2xl lg:text-lg">{name}</span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AuthorCard;
+  
--- frontend/src/components/Authors/Authors.jsx
+++ frontend/src/components/Authors/Authors.jsx
@@ -1,5 +1,36 @@
+import Subtitle from "../Common/Subtitle/Subtitle";
+import AuthorCard from "./AuthorCard";
+
+import image1 from "../../images/author1.jpg";
+import image2 from "../../images/author2.jpg";
+import image3 from "../../images/author3.jpg";
+
+const allAuthors = [
+  "Джордж Оруэлл",
+  "Эрих Мария Ремарк",
+  "Эрнест Хемингуэй",
+  "Джордж Оруэлл",
+  "Эрих Мария Ремарк",
+  "Эрнест Хемингуэй",
+  "Джордж Оруэлл",
+  "Эрих Мария Ремарк",
+  "Эрнест Хемингуэй",
+];
+
+const images = [image1, image2, image3];
+
 const Authors = () => {
-  return <div>Authors</div>;
+  return (
+    <div className="px-[12.5%] max-lg:px-0 mb-[5.5%]">
+      <Subtitle title={"Все авторы"} />
+
+      <div className="2xl:grid 2xl:grid-cols-4 flex flex-col gap-12 mt-[24px]">
+        {allAuthors.map((el, index) => (
+          <AuthorCard image={images[index % 3]} name={el} key={index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Authors;
